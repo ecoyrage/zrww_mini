ddac7dc0bb61d8ab925544efa4d683e636bee202
chenchao <537096415@qq.com>
2019-05-13 14:35:01 +0800

    搜索

--- src/mixins/utils.js
+++ src/mixins/utils.js
@@ -90,6 +90,88 @@ export default class utils extends wepy.mixin {
 
       return (num).toFixed(18).replace(/\.?0+$/, "");
     }
+    changeWorkDay(data) {
+      let day = data.split(",");
+      let str = ""
+      if (day.length == 7) {
+        return "周一 至 周日"
+      } else {
+        for (let i = 0; i < day.length; i++) {
+          if (day[i] == 1) {
+            str += "周一、"
+          }
+          if (day[i] == 2) {
+            str += "周二、"
+          }
+          if (day[i] == 3) {
+            str += "周三、"
+          }
+          if (day[i] == 4) {
+            str += "周四、"
+          }
+          if (day[i] == 5) {
+            str += "周五、"
+          }
+          if (day[i] == 6) {
+            str += "周六、"
+          }
+          if (day[i] == 7) {
+            str += "周日、"
+          }
+        }
+      }
+      return str.substring(0, str.length - 1)
+    }
+
+    isResting(day, time) {
+      let days = day.split(",");
+      let today = new Date().getDay();
+      if (today == 0) {
+        today = 7
+      }
+      if (days.indexOf(today + "") > -1) {
+        let times = time.split(",")
+        let array = new Array();
+        for (let i = 0; i < times.length; i++) {
+          let hour = times[i].split("~")
+          let start = hour[0]
+          let end = hour[1]
+          array.push(this.time_range(start, end))
+        }
+        return !this.contains(array, true);
+
+      } else {
+        return true;
+      }
+
+    }
+
+    time_range(beginTime, endTime) {
+      var strb = beginTime.split(":");
+      var stre = endTime.split(":");
+      var b = new Date();
+      var e = new Date();
+      var n = new Date();
+      b.setHours(strb[0]);
+      b.setMinutes(strb[1]);
+      e.setHours(stre[0]);
+      e.setMinutes(stre[1]);
+      if (n.getTime() - b.getTime() > 0 && n.getTime() - e.getTime() < 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    contains(a, obj) {
+      var i = a.length;
+      while (i--) {
+        if (a[i] === obj) {
+          return true;
+        }
+      }
+      return false;
+    }
  
 
     onLoad(){
